a8306d0722e802f8934293894aa48c1b67bb8d95
Lucas Bailo <lucasebailo@gmail.com>
2025-05-11 15:30:44 -0300

    Finishing course

--- ScreenSound03-projeto-inicial/ScreenSound/Program.cs
+++ ScreenSound03-projeto-inicial/ScreenSound/Program.cs
@@ -1,6 +1,18 @@
 ﻿using ScreenSound.Menus;
 using ScreenSound.Modelos;
+using OpenAI;
 
+using OpenAI.Chat;
+
+ChatClient client = new(
+    model: "gpt-4o",
+    apiKey: "sk - proj - uVmiDnynEVjozQsAG1IpAF4K9E_9TQ - LWItYUM4ibJDNfulf_FwaIq3ru0tzpnaS4V_7_xS7dpT3BlbkFJpuMXVBZolDv_6nGa4T_VGKrSpmQ587taqSXuovycOL5E67pJQXOHRX6qgYOFIgyc--dtum3_wA");
+
+ChatCompletion completion = client.CompleteChat("Resuma a banda Seether em 1 parágrafo. Adote um estilo informal.");
+Console.WriteLine(completion);
+
+
+Console.WriteLine($"Resposta: {completion.Content[0].Text}");
 Banda ira = new Banda("Ira!");
 ira.AdicionarNota(new Avaliacao(10));
 ira.AdicionarNota(new Avaliacao(8));
@@ -16,7 +28,8 @@ opcoes.Add(1, new MenuRegistrarBanda());
 opcoes.Add(2, new MenuRegistrarAlbum());
 opcoes.Add(3, new MostrarBandasRegistradas());
 opcoes.Add(4, new MenuAvaliarBanda());
-opcoes.Add(5, new MenuExibirDetalhes());
+opcoes.Add(5, new MenuAvaliarAlbum());
+opcoes.Add(6, new MenuExibirDetalhes());
 opcoes.Add(-1, new MenuSair());
 
 void ExibirLogo()
@@ -40,7 +53,8 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 2 para registrar o álbum de uma banda");
     Console.WriteLine("Digite 3 para mostrar todas as bandas");
     Console.WriteLine("Digite 4 para avaliar uma banda");
-    Console.WriteLine("Digite 5 para exibir os detalhes de uma banda");
+    Console.WriteLine("Digite 5 para avaliar uma álbum");
+    Console.WriteLine("Digite 6 para exibir os detalhes de uma banda");
     Console.WriteLine("Digite -1 para sair");
 
     Console.Write("\nDigite a sua opção: ");
